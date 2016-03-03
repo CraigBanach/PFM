0895a1e07f3b200251e00f0fb1ea5d0b5d300c8c
Craig Banach <Craig Banach>
2016-03-03 21:17:57 +0000

    Testing the HTTP POST request

--- PFM.go
+++ PFM.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 func main() {
-	res, err := http.Post("http://posttestserver.com/post.php", "application/x-www-form-urlencoded", url.QueryEscape("Authorization: oauth_callback=http://www.bbc.co.uk/news"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	robots, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	apiUrl := "https://apisandbox.openbankproject.com"
+	resource := "/oauth/initiate"
+	data := url.Values{}
+	data.Set("oauth_callback", "http://www.bbc.co.uk/news")
+
+	u, _ := url.ParseRequestURI(apiUrl)
+	u.Path = resource
+	urlStr := fmt.Sprintf("%v", u) // "https://api.com/user/"
+
+	client := &http.Client{}
+	r, _ := http.NewRequest("POST", urlStr, bytes.NewBufferString(data.Encode())) // <-- URL-encoded payload
+	r.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	resp, _ := client.Do(r)
+	robots, _ := ioutil.ReadAll(resp.Body)
 	fmt.Printf("%s", robots)
-	fmt.Printf("%s", url.QueryEscape("Authorization: oauth_callback=http://www.bbc.co.uk/news"))
 }
